5203cebdd322766b9ac7dac9d2d240e3caea8768
mattebicu <mattebicu@gmail.com>
2026-04-29 23:03:13 +0200

    Aggiunto blocco totale della cache su page.js

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: "v4he4yjk", // ID corretto dal tuo screenshot
+  projectId: "xvua41ou", // ID corretto dal tuo screenshot
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false, 
